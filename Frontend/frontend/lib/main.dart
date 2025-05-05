b423af74ea51ad0b21d7ad73209aad65e06eb60b
Caprice <148072440+Jasonbs1@users.noreply.github.com>
2025-05-05 23:31:28 +0800

    Update main.dart

--- Frontend/frontend/lib/main.dart
+++ Frontend/frontend/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/login.dart';
+import 'package:frontend/register.dart';
+import 'package:frontend/dashboard.dart';
 
 void main() {
   runApp(MaterialApp(
