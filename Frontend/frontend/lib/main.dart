7508adf65a168f50b6c30f68f54ef547b41850c0
Jason Bintang Setiawan <148072440+Caprizziest@users.noreply.github.com>
2025-05-14 00:01:33 +0800

    Add files via upload

--- Frontend/frontend/lib/main.dart
+++ Frontend/frontend/lib/main.dart
@@ -1,14 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/login.dart';
-import 'package:frontend/register.dart';
-import 'package:frontend/dashboard.dart';
+// import 'package:frontend/register.dart';
+// import 'package:frontend/dashboard.dart';
 
-void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    theme: ThemeData(
-      fontFamily: 'Poppins',
-    ),
-    home: const login(),
-  ));
+import 'package:sentry_flutter/sentry_flutter.dart';
+
+
+
+Future<void> main() async {
+  await SentryFlutter.init(
+    (options) {
+      options.dsn = 'https://0b83a48effc377ba8219a49ea3e759ee@o4509316345102336.ingest.us.sentry.io/4509316481417216';
+      // Adds request headers and IP for users,
+      // visit: https://docs.sentry.io/platforms/dart/data-management/data-collected/ for more info
+      options.sendDefaultPii = true;
+    },
+    appRunner: () => runApp(const MyApp()),
+  );
 }
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'Poppins',
+      ),
+      home: const login(),
+    );
+  }
+}
+
+
+
+// void main() {
+//   runApp(MaterialApp(
+//     debugShowCheckedModeBanner: false,
+//     theme: ThemeData(
+//       fontFamily: 'Poppins',
+//     ),
+//     home: const login(),
+//   ));
+// }
