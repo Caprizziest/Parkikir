f1db6a4ccd01f347f2adecea91649685226b15ad
Jason Bintang Setiawan <148072440+Caprizziest@users.noreply.github.com>
2025-05-25 21:12:14 +0800

    changed password to root

--- Backend/parkirki/parkirki/settings.py
+++ Backend/parkirki/parkirki/settings.py
@@ -119,7 +119,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'parkirki',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'root',
         'HOST': '127.0.0.1',
         'PORT': '3306'
     }
