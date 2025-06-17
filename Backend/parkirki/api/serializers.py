6f454dd93c184a1e4fc09ce85b803b9cbd6a8e61
Aaron Jevon Benedict Kongdoh <abenedict01@student.ciputra.ac.id>
2025-06-18 01:43:54 +0800

    Update serializers.py

--- Backend/parkirki/api/serializers.py
+++ Backend/parkirki/api/serializers.py
@@ -7,7 +7,7 @@ import base64
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email']
+        fields = ['id', 'username', 'email', 'is_staff']
 
 
 class RegisterSerializer(serializers.ModelSerializer):
