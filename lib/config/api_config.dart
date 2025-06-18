1df0b9585ea59ad8d1ddd881438fc1ccdf08ef80
Jason Bintang Setiawan <148072440+Caprizziest@users.noreply.github.com>
2025-06-18 17:19:48 +0800

    Add files via upload

---
+++ lib/config/api_config.dart
@@ -0,0 +1,40 @@
+class ApiConfig {
+  // Base URL
+  static const String baseUrl = 'http://10.1.49.143:8000/api';
+  static const String wsBaseUrl = 'ws://10.1.49.143:8000/ws';
+
+  // API Endpoints - centralized
+  static const String loginEndpoint = '/login/';
+  static const String registerEndpoint = '/register/';
+  static const String refreshTokenEndpoint = '/token/refresh/';
+  static const String logoutEndpoint = '/logout/';
+  static const String paymentCreateEndpoint = '/payment/create/';
+  static const String userDetailEndpoint = '/users/';
+  static const String slotParkirListCreateEndpoint = '/slotparkir/';
+  static String slotParkirDetailEndpoint(String pk) => '/slotparkir/$pk/';
+  static String slotParkirUpdateStatusEndpoint(String pk) => '/slotparkir/$pk/status/';
+
+
+  // WebSocket Endpoints
+  static const String parkingWebSocketEndpoint = '/parkiran/';
+
+  // Full URLs
+  static String get loginUrl => '$baseUrl$loginEndpoint';
+  static String get registerUrl => '$baseUrl$registerEndpoint';
+  static String get refreshTokenUrl => '$baseUrl$refreshTokenEndpoint';
+  static String get logoutUrl => '$baseUrl$logoutEndpoint';
+  static String get paymentCreateUrl => '$baseUrl$paymentCreateEndpoint';
+  static String userDetailUrl(int userId) =>
+      '$baseUrl$userDetailEndpoint$userId/';
+  static String get slotParkirListCreateUrl => '$baseUrl$slotParkirListCreateEndpoint';
+  static String slotParkirDetailUrl(String pk) => '$baseUrl${slotParkirDetailEndpoint(pk)}';
+  static String slotParkirUpdateStatusUrl(String pk) => '$baseUrl${slotParkirUpdateStatusEndpoint(pk)}';
+
+  // WebSocket URLs
+  static String get parkingWebSocketUrl =>
+      '$wsBaseUrl$parkingWebSocketEndpoint';
+
+  // Timeouts
+  static const Duration connectionTimeout = Duration(seconds: 30);
+  static const Duration receiveTimeout = Duration(seconds: 30);
+}
